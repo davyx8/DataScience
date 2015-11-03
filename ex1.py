5558d568fa22a0ad363a3d2eb309d56d4e14609a
David Pash <davyx8@cs.huji.ac.il>
2015-11-03 10:54:08 +0200

    dataaaa2

--- ex1.py
+++ ex1.py
@@ -1,4 +1,5 @@
 import gzip
+import math
 from collections import defaultdict
 from random import random
 
@@ -18,6 +19,17 @@ def jacCalc(ratingsA,ratingsB):
 				C+=1
 	return (float(C)/(len(ratingsA)+len(ratingsB)))
 
+
+def cosine_similarity(v1,v2):
+    "compute cosine similarity of v1 to v2: (v1 dot v2)/{||v1||*||v2||)"
+    sumxx, sumxy, sumyy = 0, 0, 0
+    for i in range(len(v1)):
+        x = v1[i]; y = v2[i]
+        sumxx += x*x
+        sumyy += y*y
+        sumxy += x*y
+    return sumxy/math.sqrt(sumxx*sumyy)
+
 ratings=[]
 ratingsA=[]
 ratingsB=[]
@@ -26,11 +38,15 @@ B=0
 sizes={}
 inter={}
 objects=[]
+objects2=[]
 scores={}
-for l in readGz("/tmp/train.json.gz"):
+longRating={}
+for l in readGz("/cs/stud/davyx8/.++/train2.json.gz"):
   inter[l['reviewerID']]=0
   sizes[l['reviewerID']]=0
+  longRating[l['reviewerID']]={}
   ratings.append((l['reviewerID'], l['itemID'], l['rating']))
+  
   nUser[l['reviewerID']] += 1
   nItem[l['itemID']] += 1
   if (l['reviewerID'] == 'U229891973'):
@@ -39,32 +55,59 @@ for l in readGz("/tmp/train.json.gz"):
   	A+= 1
   if (l['reviewerID'] == 'U622491081'):
   	ratingsB.append((l['reviewerID'], l['itemID'], l['rating']))
+  	objects2.append((l['itemID'],l['rating']))
   	objects.append(l['itemID'])
   	B+= 1
 
 
 
 
-print(objects)
 print("The jaccard of U229891973 and U622491081:")
 print(jacCalc(ratingsA,ratingsB))
 maxJac=0
-
+idx=0
 for tmp in ratings:
 	sizes[tmp[0]]=sizes[tmp[0]]+1
 	if (tmp[1] in objects):
 		inter[tmp[0]]=inter[tmp[0]]+1
-
+		#tmp2=longRating[tmp[0]
+		#tmp2['aa']=l[tmp[2]]
+		longRating[tmp[0]].update({tmp[1]:tmp[2]})
+		
+		print(longRating[tmp[0]])
 	scores[tmp[0]]=float(inter[tmp[0]])/sizes[tmp[0]]
-for i in inter:
-	if inter[i]>0:
-		print(inter[i])
 
-for i in scores:
-	if scores[i]>0:
-		print(i)
-		print(scores[i])
 
+#for i in scores:
+#	if scores[i]>maxJac:
+#		maxJac=scores[i]
+#print("Users with highest score")
+#for i in scores:
+#	if scores[i]==maxJac:
+#		print i
+
+#Cosine similarity part
+cosinescores=[]
+vec=[0]*len(objects2)
+i=0
+scoresvec=[0]*len(objects2)
+for x in (objects2):
+	print(x)
+	print(x[0])
+	print(objects2[i][1])
+	vec[i]=x[0]
+	i=i+1
+	scoresvec[i]=objects2[i][1]
+
+print(vec)
+tmpvec=[0]*len(objects2)
+for tmp in longRating:
+
+	for i in longRating[tmp]:
+		idx=vec.index(i)
+		tmpvec[idx]=longRating[tmp][i]
+		print(tmpvec)
+		print(cosine_similarity(tmpvec,scoresvec))
 
 
 
