5c5a9930a88a3d11980ab73e821498329cc73cba
David Pash <davyx8@cs.huji.ac.il>
2015-11-03 14:05:08 +0200

    dataaaa4

--- ex1.py
+++ ex1.py
@@ -1,8 +1,17 @@
 import gzip
 import math
+
+
+
 from collections import defaultdict
 from random import random
-
+def median(l):
+    half = len(l) // 2
+    l.sort()
+    print(l)
+    if not len(l) % 2:
+        return (l[half - 1] + l[half]) / 2.0
+    return l[half]
 nUser = defaultdict(int)
 nItem = defaultdict(int)
 def readGz(f):
@@ -15,9 +24,8 @@ def jacCalc(ratingsA,ratingsB):
 	for k in ratingsA:
 		for z in ratingsB:
 			if (k[1] == z[1]):
-
 				C+=1
-	return (float(C)/(len(ratingsA)+len(ratingsB)))
+	return (float(C)/(len(ratingsA)+len(ratingsB)-C))
 
 
 def cosine_similarity(v1,v2):
@@ -34,19 +42,23 @@ ratings=[]
 ratingsA=[]
 ratingsB=[]
 A=0
-B=0
+
 sizes={}
 inter={}
 objects=[]
 objects2=[]
 scores={}
 longRating={}
-for l in readGz("/cs/stud/davyx8/.++/train2.json.gz"):
+cosinescores={}
+helpful=[]
+maxJac=0
+idx=0
+for l in readGz("/cs/stud/davyx8/.++/train.json.gz"):
   inter[l['reviewerID']]=0
   sizes[l['reviewerID']]=0
   longRating[l['reviewerID']]={}
   ratings.append((l['reviewerID'], l['itemID'], l['rating']))
-  
+  helpful.append(l['helpful'])
   nUser[l['reviewerID']] += 1
   nItem[l['itemID']] += 1
   if (l['reviewerID'] == 'U229891973'):
@@ -57,58 +69,85 @@ for l in readGz("/cs/stud/davyx8/.++/train2.json.gz"):
   	ratingsB.append((l['reviewerID'], l['itemID'], l['rating']))
   	objects2.append((l['itemID'],l['rating']))
   	objects.append(l['itemID'])
-  	B+= 1
 
 
 
 
 print("The jaccard of U229891973 and U622491081:")
 print(jacCalc(ratingsA,ratingsB))
-maxJac=0
-idx=0
+
 for tmp in ratings:
 	sizes[tmp[0]]=sizes[tmp[0]]+1
 	if (tmp[1] in objects):
 		inter[tmp[0]]=inter[tmp[0]]+1
-		#tmp2=longRating[tmp[0]
-		#tmp2['aa']=l[tmp[2]]
 		longRating[tmp[0]].update({tmp[1]:tmp[2]})
 		
-		print(longRating[tmp[0]])
-	scores[tmp[0]]=float(inter[tmp[0]])/sizes[tmp[0]]
+	# jacc score calc, (inters/unifi)
+	#i didnt use the function cause it was easier this way
+	scores[tmp[0]]=float(inter[tmp[0]])/(sizes[tmp[0]]+len(objects)-inter[tmp[0]])
 
 
-#for i in scores:
-#	if scores[i]>maxJac:
-#		maxJac=scores[i]
-#print("Users with highest score")
-#for i in scores:
-#	if scores[i]==maxJac:
-#		print i
+for i in scores:
+	if scores[i]>maxJac:
+		maxJac=scores[i]
+print("Users with highest score")
+for i in scores:
+	if scores[i]==maxJac:
+		print i
 
 #Cosine similarity part
-cosinescores=[]
-vec=[0]*len(objects2)
+vec=[0]*len(objects)
+scoresvec=[0]*len(objects)
 i=0
-scoresvec=[0]*len(objects2)
-for x in (objects2):
-	print(x)
-	print(x[0])
-	print(objects2[i][1])
-	vec[i]=x[0]
-	i=i+1
-	scoresvec[i]=objects2[i][1]
 
-print(vec)
+for x in (objects2):
+	vec[i]=x[0]
+	scoresvec[i]=objects2[i][1]
+	i=i+1
+
 tmpvec=[0]*len(objects2)
 for tmp in longRating:
 
 	for i in longRating[tmp]:
 		idx=vec.index(i)
 		tmpvec[idx]=longRating[tmp][i]
-		print(tmpvec)
+	try:
+		cosinescores[tmp]=cosine_similarity(tmpvec,scoresvec)
+		#print(cosine_similarity(tmpvec,scoresvec))
+	except ZeroDivisionError:
+		cosinescores[tmp]=0
+	if(tmp=='U229891973'):
+		print("The cosine similarity of U229891973 and U622491081:")
 		print(cosine_similarity(tmpvec,scoresvec))
 
+maxcos=0
+for i in cosinescores:
+	if cosinescores[i]>maxcos:
+		maxcos=cosinescores[i]
+print("Users with highest  cosine score")
+for i in cosinescores:
+	if cosinescores[i]==maxcos:
+		print i
 
 
+#suggestion for helpful calculation
+# number of (helpful ratio*min(1, #number of rate/average number of rates)
+hlpVec=[0]*len(ratings)
+hlpScore=[0]*len(ratings)
+i=0
+for hlp in helpful :
+	hlpVec[i]=hlp['outOf']
+	hlpScore[i]=hlp['nHelpful']/float(hlp['outOf'])
+	i+=1
 
+
+average=sum(hlpVec)/float(len(hlpVec))
+print(average)
+
+
+for x in range(len(hlpScore)):
+	print(hlpScore[x]*min(1,hlpVec[x]/float(average)) )
+
+	hlpScore[x]=hlpScore[x]*min(1,hlpVec[x]/float(average)) 
+
+print(hlpScore)
