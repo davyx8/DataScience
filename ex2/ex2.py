1fb8beadc36663d903d94ae42fe2dadb433c0bc8
David Pash <davyx8@cs.huji.ac.il>
2015-11-24 23:09:54 +0200

    almost done ex2

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -21,8 +21,6 @@ for e in urllib.urlopen("fbegonet.txt"):
   nodes.add(x)
   nodes.add(y)
 
-len(edges)
-len(nodes)
 
 G = nx.Graph()
 for e in edges:
@@ -30,12 +28,64 @@ for e in edges:
 nx.draw(G)
 plt.show()
 plt.clf()
+G2 = nx.Graph()
+
 
 connectedComponents = list(nx.connected_components(G))
-len(connectedComponents)
-len(connectedComponents[0])
 
-### Find all 3 and 4-cliques in the graph ###
+for gr in connectedComponents:
+  print(len(gr))
+print(len(connectedComponents[0]))
+w=len(connectedComponents[0])
+print(len(connectedComponents))
+
+print(connectedComponents[0])
+sortedarr=sorted(connectedComponents[0])
+
+
+def splitter(A):
+    B = A[0:len(A)//2]
+    C = A[len(A)//2:]
+
+    return (B,C)
+
+B,C=splitter(sortedarr)
+
+def norm_cut(B,C,edges):
+  cut=0
+  volB=0
+  volC=0
+  for e in edges:
+    x,y = e[0],e[1]
+    x = int(x)
+    y = int(y)
+    if( (x in  connectedComponents[0])  and (y in connectedComponents[0] )):
+      G2.add_edge(e[0],e[1])
+      if ((x in B) and (y in C)):
+        cut=cut+1
+      if ((y in B)):
+        volB=volB+1
+      if ((x in C)):
+        volC=volC+1
+  return (float(cut)/volB+float(cut)/volC)
+
+
+nx.draw(G2)
+plt.show()
+plt.clf()
+print('the initial normalized cut is: ')
+print(norm_cut(B,C,edges))
+
+
+
+
+import networkx as nx
+import numpy as np
+import logging
+
+
+'''
+
 cliques3 = set()
 cliques4 = set()
 for n1 in nodes:
@@ -58,14 +108,4 @@ for n1 in nodes:
 c = list(nx.k_clique_communities(G,4))
 print(len(c))
 print(len(c[0]))
-
-# Given a clique size: 4
-# Initialize every clique as it's own community - cliques4()
-
-for x in cliques4:
-	for y in cliques4:
-		for z in cliques3:
-			if (z in x and z in y):
-				x1 = x + y
-				y = y + x
-				x = x1
+''' 
